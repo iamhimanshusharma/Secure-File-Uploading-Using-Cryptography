919676e06c0fd39af3116e70d0631a61db6d0dc6
Himanshu Sharma <53050313+iamhimanshusharma@users.noreply.github.com>
2021-08-13 08:55:00 +0530

    Add files via upload

--- encdec/algo.py
+++ encdec/algo.py
@@ -1,5 +1,6 @@
 from Crypto import Random
 from Crypto.Cipher import AES
+from Crypto.Cipher import DES
 from cryptography.fernet import Fernet
 import os
 import os.path
@@ -26,8 +27,6 @@ class Encryptor:
         enc = self.encrypt(plaintext, self.key)
         with open(file_name + ".enc", 'wb') as fo:
             fo.write(enc)
-        # os.remove(file_name)
-        # return hello
 
     def decrypt(self, ciphertext, key):
         iv = ciphertext[:AES.block_size]
@@ -41,11 +40,11 @@ class Encryptor:
         dec = self.decrypt(ciphertext, self.key)
         with open(file_name[:-4], 'wb') as fo:
             fo.write(dec)
-        # os.remove(file_name)
         return file_name[:-4]
 
-# <<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-# ?????????????????????????????????????????????????????????????????????????????????/?
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# ====================FERNET ALGORITHM===================================================
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
     def fernet_encrypt(self, message, key):
         f = Fernet(self.key)
@@ -69,8 +68,23 @@ class Encryptor:
         with open(file_name[:-4], 'wb') as fo:
             fo.write(enc)
 
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# ====================DES ALGORITHM===================================================
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+    def des_pad(self, s):
+        return s + b"\0" * (8 - len(s) % 8)
+
+    def des_encrypt(self, message, key):
+        cipher = DES.new(self.key, DES.MODE_ECB)
+        return cipher.encrypt(self.des_pad(message))
+
+    def des_decrypt(self, message, key):
+        cipher = DES.new(self.key, DES.MODE_ECB)
+        return cipher.decrypt(message).rstrip(b"\0")
+
+
     def getAllFiles(self, path):
-        # dir_path = os.path.dirname(os.path.realpath(__file__))
         dirs = []
         for dirName, subdirList, fileList in os.walk(path):
             for fname in fileList:
@@ -87,57 +101,3 @@ class Encryptor:
         dirs = self.getAllFiles(path)
         for file_name in dirs:
             self.decrypt_file(file_name)
-
-
-# Fernet Encryption
-    
-
-
-# key = b'[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\x94\x9d(\x9e'
-# enc = Encryptor(key)
-# clear = lambda: os.system('cls')
-
-# if os.path.isfile('data.txt.enc'):
-#     while True:
-#         password = str(input("Enter password: "))
-#         enc.decrypt_file("data.txt.enc")
-#         p = ''
-#         with open("data.txt", "r") as f:
-#             p = f.readlines()
-#         if p[0] == password:
-#             enc.encrypt_file("data.txt")
-#             break
-
-#     while True:
-#         clear()
-#         choice = int(input(
-#             "1. Press '1' to encrypt file.\n2. Press '2' to decrypt file.\n3. Press '3' to Encrypt all files in the directory.\n4. Press '4' to decrypt all files in the directory.\n5. Press '5' to exit.\n"))
-#         clear()
-#         if choice == 1:
-#             enc.encrypt_file(str(input("Enter name of file to encrypt: ")))
-#         elif choice == 2:
-#             enc.decrypt_file(str(input("Enter name of file to decrypt: ")))
-#         elif choice == 3:
-#             enc.encrypt_all_files()
-#         elif choice == 4:
-#             enc.decrypt_all_files()
-#         elif choice == 5:
-#             exit()
-#         else:
-#             print("Please select a valid option!")
-
-# else:
-#     while True:
-#         clear()
-#         password = str(input("Setting up stuff. Enter a password that will be used for decryption: "))
-#         repassword = str(input("Confirm password: "))
-#         if password == repassword:
-#             break
-#         else:
-#             print("Passwords Mismatched!")
-#     f = open("data.txt", "w+")
-#     f.write(password)
-#     f.close()
-#     enc.encrypt_file("data.txt")
-#     print("Please restart the program to complete the setup")
-#     time.sleep(15)
--- encdec/views.py
+++ encdec/views.py
@@ -8,11 +8,6 @@ import shutil
 from hashlib import md5, sha256
 from datetime import datetime
 
-
-# def split_cb(f, s):
-#     print("file: {0}, size: {1}".format(f, s))
-
-
 # Create your views here.
 def index(request):
     folders = []
@@ -30,31 +25,93 @@ def uploads(request):
         real_key = request.POST.get('keyed')
         key_mode = request.POST.get('key')
 
+        aes = []
+        fernet = []
+        des = []
+        media_list = ['jpeg', '.jpg', '.png', '.gif', '.mkv', '.mp4']
+
         if key_mode == 'dflt':
             key = b'[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\x94\x9d(\x9e'
             fernet_key = b'skZ9uZYBJXOOnbkZeCA2vMeoECm_Z0bVRDKS6ofxOrc='
+            des_key = b'fYHBWInR'
         elif key_mode == 'kinp':
             key = bytes(md5(real_key.encode("ascii")).hexdigest(), 'utf-8')
             f_key = sha256(real_key.encode('ascii')).hexdigest()
-            fernet_key = f_key[:44]
-        
+            fer_key = f_key[:43]+"="
+            fernet_key = bytes(fer_key, 'utf-8')
+            des_key = bytes(f_key[:8], 'utf-8')
+     
         hello = Encryptor(key)
-        plaintext = files.read()
-        enc = hello.encrypt(plaintext, key)
-        folder_name = str(files)[:-4]+"_"+timestamp
-        path = 'media/encdec/files/'+folder_name
-        os.mkdir(path)
-        with open(path+ "/"+str(files) + ".enc", 'wb') as fo:
-            fo.write(enc)
-        datt = Product(product_folder=folder_name,product_type=file_type, key_type=key_mode, product_name=str(files)+".enc")
-        datt.save()
-        fs.split(file=path+ '/'+str(files) + '.enc', split_size=100, output_dir=path+ '/')
-        os.remove(path+'/'+str(files)+".enc")
-        remover_file = hello.getAllFiles(path)
-        hello.encrypt_all_files(path)
-        print(remover_file)
-        for f in remover_file:
-            os.remove(f)
+        hello_f = Encryptor(fernet_key)
+        hello_d = Encryptor(des_key)
+
+        if str(files)[-4:] in media_list:#== 'jpeg' or str(files)[-3:] == 'png' or str(files)[-3:] == 'jpg' or str(files)[-3:] == 'ico'  or str(files)[-3:] == 'mp4':
+            plaintext = files.read()
+            folder_name = str(files)[:-4]+"_"+timestamp
+            path = 'media/encdec/files/'+folder_name
+            os.mkdir(path)
+            with open(path+ "/"+str(files), 'wb') as fo:
+                fo.write(plaintext)
+            datt = Product(product_folder=folder_name,product_type=file_type, key_type=key_mode, product_name=str(files)+".enc")
+            datt.save()
+            # fs.split(file=path+ '/'+str(files), split_size=1000, output_dir=path+ '/')
+            # remover_file = hello.getAllFiles(path)
+
+            with open(path +"/"+str(files), 'rb') as fo:
+                plaintext = fo.read()
+            enc = hello.encrypt(plaintext, key)
+            with open(path +"/"+str(files) + ".enc", 'wb') as fo:
+                fo.write(enc)
+            os.remove(path +"/"+str(files))
+
+        else:
+            plaintext = files.read()
+            folder_name = str(files)[:-4]+"_"+timestamp
+            path = 'media/encdec/files/'+folder_name
+            os.mkdir(path)
+            with open(path+ "/"+str(files), 'wb') as fo:
+                fo.write(plaintext)
+            datt = Product(product_folder=folder_name,product_type=file_type, key_type=key_mode, product_name=str(files)+".enc")
+            datt.save()
+            fs.split(file=path+ '/'+str(files), split_size=1000, output_dir=path+ '/')
+            remover_file = hello.getAllFiles(path)
+
+            for i in range(0, len(remover_file), 3):
+                aes.append(remover_file[i])
+
+            for j in range(1, len(remover_file), 3):
+                fernet.append(remover_file[j])
+
+            for x in range(2, len(remover_file), 3):
+                des.append(remover_file[x])
+
+            if len(aes)>0:
+                for k in aes:
+                    with open(k, 'rb') as fo:
+                        plaintext = fo.read()
+                    enc = hello.encrypt(plaintext, key)
+                    with open(k + ".aenc", 'wb') as fo:
+                        fo.write(enc)
+                    os.remove(k)
+
+            if len(fernet)>0:
+                for l in fernet:
+                    with open(l, 'rb') as fo:
+                        plaintext = fo.read()
+                    enc = hello_f.fernet_encrypt(plaintext, fernet_key)
+                    with open(l + ".fenc", 'wb') as fo:
+                        fo.write(enc)
+                    os.remove(l)
+
+            if len(des)>0:
+                for y in des:
+                    with open(y, 'rb') as fo:
+                        plaintext = fo.read()
+                    enc = hello_d.des_encrypt(plaintext, des_key)
+                    with open(y + ".denc", 'wb') as fo:
+                        fo.write(enc)
+                    os.remove(y)
+
         thank=True
         params = {'thank':thank}
         return render(request, 'encdec/upload.html', params)
@@ -64,29 +121,80 @@ def dec(request):
     fs = Filesplit()
     files = request.GET.get('product_n')
     folder = request.GET.get('product_f')
+    aes = []
+    fernet = []
+    des = []
+    media_list = ['jpeg.enc', '.jpg.enc', '.png.enc', '.gif.enc', '.mkv.enc', '.mp4.enc']
 
     if request.GET.get('keyed'):
         real_key = request.GET.get('keyed')
         key = bytes(md5(real_key.encode("ascii")).hexdigest(), 'utf-8')
+        f_key = sha256(real_key.encode('ascii')).hexdigest()
+        fer_key = f_key[:43]+"="
+        fernet_key = bytes(fer_key, 'utf-8')
+        des_key = bytes(f_key[:8], 'utf-8')
     else:
         key = b'[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\x94\x9d(\x9e'
-    
+        fernet_key = b'skZ9uZYBJXOOnbkZeCA2vMeoECm_Z0bVRDKS6ofxOrc='
+        des_key = b'fYHBWInR'
+        
+
     hello = Encryptor(key)
+    hello_f = Encryptor(fernet_key)
+    hello_d = Encryptor(des_key)
+
     file_path = 'media/encdec/files/'+folder+"/"
-    hello.decrypt_all_files(file_path)
-    fs.merge(file_path)
-    with open(file_path + files, 'rb') as fo:
-        ciphertext = fo.read()
-    dec = hello.decrypt(ciphertext, key)
-    with open(file_path + files[:-4], 'wb') as fo:
-        fo.write(dec)
-    removing_file = hello.getAllFiles(file_path)
-    for f in removing_file:
-        if f == file_path + '\\' + files[:-4]:
-            continue
-        os.remove(f)
+    all_files = os.listdir(file_path)
+
+    if str(files)[-8:] in media_list:#== ".jpeg.enc" or str(files)[-8:] == ".png.enc" or str(files)[-8:] == ".jpg.enc"  or str(files)[-8:] == ".ico.enc"  or str(files)[-8:] == ".mp4.enc":
+        for k in all_files:
+            with open(file_path + k, 'rb') as fo:
+                enctext = fo.read()
+            dec = hello.decrypt(enctext, key)
+            with open(file_path + k[:-4], 'wb') as fo:
+                fo.write(dec)
+            os.remove(file_path + k)
+
+    else: 
+        for i in all_files:
+            if i[-5:] == ".aenc":
+                aes.append(i)
+            elif i[-5:] == ".fenc":
+                fernet.append(i)
+            else:
+                des.append(i)
+        
+        for k in aes:
+            with open(file_path + k, 'rb') as fo:
+                enctext = fo.read()
+            dec = hello.decrypt(enctext, key)
+            with open(file_path + k[:-5], 'wb') as fo:
+                fo.write(dec)
+            os.remove(file_path + k)
+
+        for l in fernet:
+            with open(file_path + l, 'rb') as fo:
+                enctext = fo.read()
+            plaintext = hello_f.fernet_decrypt(enctext, fernet_key)
+            with open(file_path + l[:-5], 'wb') as fo:
+                fo.write(plaintext)
+            os.remove(file_path + l)
+
+        for y in des:
+            with open(file_path + y, 'rb') as fo:
+                enctext = fo.read()
+            plaintext = hello_d.des_decrypt(enctext, des_key)
+            with open(file_path + y[:-5], 'wb') as fo:
+                fo.write(plaintext)
+            os.remove(file_path + y)
+
+        fs.merge(file_path)
+        removing_file = hello.getAllFiles(file_path)
+        for f in removing_file:
+            if f == file_path + '\\' + files[:-4]:
+                continue
+            os.remove(f)
     datt = Product.objects.filter(product_folder=folder).update(product_type="TXT", product_name=files[:-4])
-    # datt.save()
     params = {'folder':folder ,'file':files[:-4]}
     return render(request, 'encdec/download.html', params)
 
@@ -94,7 +202,6 @@ def dec(request):
 def again_uploads(request):
     fs = Filesplit()
     if request.method == 'POST':
-        # files = request.FILES['files']
         file_type = request.POST.get('file_type')
         real_key = request.POST.get('keyed')
         key_mode = request.POST.get('key')
@@ -102,35 +209,75 @@ def again_uploads(request):
         folder_file = request.POST.get('pro_file')
         folder_name = request.POST.get('folder_name')
 
+        aes = []
+        fernet = []
+        des = []
+        media_list = ['jpeg', '.jpg', '.png', '.gif', '.mkv', '.mp4']
+
+
         if key_mode == 'dflt':
             key = b'[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\x94\x9d(\x9e'
             fernet_key = b'skZ9uZYBJXOOnbkZeCA2vMeoECm_Z0bVRDKS6ofxOrc='
+            des_key = b'fYHBWInR'
         elif key_mode == 'kinp':
             key = bytes(md5(real_key.encode("ascii")).hexdigest(), 'utf-8')
             f_key = sha256(real_key.encode('ascii')).hexdigest()
-            fernet_key = f_key[:44]
+            fer_key = f_key[:43]+"="
+            fernet_key = bytes(fer_key, 'utf-8')
+            des_key = bytes(f_key[:8], 'utf-8')
         
         hello = Encryptor(key)
-        print(folder_path)
-        with open(folder_path + "/"+ folder_file, 'rb') as files:
-            plaintext = files.read()
-        enc = hello.encrypt(plaintext, key)
-        with open(folder_path + "/"+ folder_file + ".enc", 'wb') as fo:
-            fo.write(enc)
+        hello_f  = Encryptor(fernet_key)
+        hello_d = Encryptor(des_key)
+
+        if folder_file[-4:] in media_list:#=='.jpeg' or folder_file[-4:] == '.png' or folder_file[-4:] == '.jpg'  or folder_file[-4:] == '.ico' or folder_file[-4] == '.mp4':
+            with open(folder_path+"/"+folder_file, 'rb') as fo:
+                plaintext = fo.read()
+            enc = hello.encrypt(plaintext, key)
+            with open(folder_path+"/"+folder_file + ".enc", 'wb') as fo:
+                fo.write(enc)
+            os.remove(folder_path+"/"+folder_file)
+        else:
+            fs.split(file=folder_path + "/"+ folder_file, split_size=1000, output_dir=folder_path+ '/')
+            remover_file = hello.getAllFiles(folder_path)
+            for i in range(0, len(remover_file), 3):
+                aes.append(remover_file[i])
+
+            for j in range(1, len(remover_file), 3):
+                fernet.append(remover_file[j])
+
+            for x in range(2, len(remover_file), 3):
+                des.append(remover_file[x]) 
+
+            for k in aes:
+                with open(k, 'rb') as fo:
+                    plaintext = fo.read()
+                enc = hello.encrypt(plaintext, key)
+                with open(k + ".aenc", 'wb') as fo:
+                    fo.write(enc)
+                os.remove(k)
+
+            for l in fernet:
+                with open(l, 'rb') as fo:
+                    plaintext = fo.read()
+                enc = hello_f.fernet_encrypt(plaintext, fernet_key)
+                with open(l + ".fenc", 'wb') as fo:
+                    fo.write(enc)
+                os.remove(l)
+
+            for y in des:
+                with open(y, 'rb') as fo:
+                    plaintext = fo.read()
+                enc = hello_d.des_encrypt(plaintext, des_key)
+                with open(y + ".denc", 'wb') as fo:
+                    fo.write(enc)
+                os.remove(y)
+
         datt = Product.objects.get(product_folder=folder_name)
         datt.product_type= file_type
         datt.key_type=key_mode
         datt.product_name=str(folder_file)+".enc"
         datt.save()
-        fs.split(file=folder_path + "/"+ folder_file + '.enc', split_size=100, output_dir=folder_path+ '/')
-        os.remove(folder_path + "/"+ folder_file+".enc")
-        remover_file = hello.getAllFiles(folder_path)
-        hello.encrypt_all_files(folder_path)
-        os.remove(folder_path + "/"+folder_file+".enc")
-        print(remover_file)
-
-        for f in remover_file:
-            os.remove(f)
         thank=True
         params = {'thank':thank}
         return render(request, 'encdec/upload.html', params)
